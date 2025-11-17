aab344843af96353d26eab2c4a45624493a0218d
Luke Wong <lukewong93@gmail.com>
2025-11-17 18:15:41 +0800

    build: install commitlint packages

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"feat", // New feature
+				"fix", // Bug fix
+				"docs", // Documentation changes
+				"style", // Code style changes (formatting, missing semi-colons, etc)
+				"ref", // Code refactoring
+				"perf", // Performance improvements
+				"test", // Adding or updating tests
+				"build", // Changes to build system or dependencies
+				"ci", // CI/CD changes
+				"chore", // Other changes that don't modify src or test files
+				"revert", // Revert a previous commit
+				"exp", // Experimental changes
+				"trans", // Translation changes
+			],
+		],
+		"subject-case": [2, "never", ["upper-case"]], // Prevent starting with uppercase
+		"header-max-length": [2, "always", 100],
+	},
+};
